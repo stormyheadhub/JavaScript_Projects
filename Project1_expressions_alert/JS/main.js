63cb780a3008e1a6a3ba139becd18fb550b63b4a
Kimberly Sebastian <167662541+stormyheadhub@users.noreply.github.com>
2024-06-19 16:37:59 -0700

    commit

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -11,6 +11,8 @@ document.write(B);
 var Sent1="This is the beginning of the string";
 var Sent2="This is the end of the string";
 
+//because I cant get GitHub to refresh this stupid file Im writing this note
+
 document.write(Sent1 + Sent2)
 
 Document.write(2+3);
