7d3c5b871ee0f58814a103d76854ee8c00a21ba9
Kimberly Sebastian <167662541+stormyheadhub@users.noreply.github.com>
2024-06-19 16:25:31 -0700

    commit

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -20,7 +20,7 @@ function division() {
 
 function more_Math() {
     var simple_Math = (1+2) * 10 / 2-5;
-    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + this is really dumb 
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math; 
 }
 
 function modulus_Operator() {
