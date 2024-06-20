6265fff70f187569eaf55ff5206b00f5cf57041e
Kimberly Sebastian <167662541+stormyheadhub@users.noreply.github.com>
2024-06-19 23:31:44 -0700

    commit

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,21 @@
+document.write("5" + 10);
+
+document.write(2E310);
+
+document.write(-3E310);
+
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function my_Isnan() {
+    document.getElementById("Test").innerHTML = isNaN('This is a string');
+}
+
+function my_Isnanagain() {
+    document.getElementById("Test").innerHTML = isNaN('007');
+}
+
+//Use ==, ===, <,>,&&,||, and ! 
+
+
