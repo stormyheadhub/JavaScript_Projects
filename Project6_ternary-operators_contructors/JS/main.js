d31344dc600b8fa7f1ee3726fa14c1336cbb2ea9
Kimberly Sebastian <167662541+stormyheadhub@users.noreply.github.com>
2024-06-23 19:23:37 -0700

    commit

--- Project6_ternary-operators_contructors/JS/main.js
+++ Project6_ternary-operators_contructors/JS/main.js
@@ -1,10 +1,27 @@
+//This is an example of using a function
 function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;   
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+//This is a keyword constructor function
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
 
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
+}
+//This is a nested function. The Plus_one function is nested inside the count_Function
 function count_Function() {
     document.getElementById("Counting").innerHTML = Count();
     function Count() {
@@ -15,3 +32,4 @@ function count_Function() {
     }
 }
 
+//create a function to display the results
