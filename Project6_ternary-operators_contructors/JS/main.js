44e22e872b2de8183aaf2f337f604c6fbd8e8f49
Kimberly Sebastian <167662541+stormyheadhub@users.noreply.github.com>
2024-06-24 17:55:03 -0700

    commit

--- Project6_ternary-operators_contructors/JS/main.js
+++ Project6_ternary-operators_contructors/JS/main.js
@@ -26,7 +26,7 @@ function count_Function() {
     document.getElementById("Counting").innerHTML = Count();
     function Count() {
         var Starting_point = 9;
-        function Plus_one {Starting_point += 1;}
+        function Plus_one() {Starting_point += 1;}
         Plus_one();
         return Starting_point;
     }
